2de5ee757f47e933ae148e933fab1670ee7214df
linh <phamhoailinh779@gmail.com>
2023-02-24 14:12:58 +0700

    form login

--- giaodien/login.php
+++ giaodien/login.php
@@ -13,25 +13,48 @@
         else header('location: login.php');
     }
 ?>
+<!-- login form -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Admin</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>Login | Ludiflex - youtube channel</title>
 </head>
 <body>
-    <div class="main">
-        <h2>Login</h2>
-        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-            <label>Tên đăng nhập:</label>
-            <input type="text" name="user" id="">
-            <label>Mật khẩu:</label>
-            <input type="text" name="pass" id="">
-            <button type="submit" name="dangnhap" id="">Đăng nhập</button>
-        </form>
+    <div class="wrapper">
+        <div class="container main">
+            <div class="row">
+                <div class="col-md-6 side-image">
+                    <!-------Image-------->
+                    <img src="images/loginlogo.png" alt="">
+                    
+                </div>
+                <div class="col-md-6 right">
+                     <div class="input-box">
+                        <header>Create account</header>
+                        <div class="input-field">
+                            <input type="text" class="input" id="email" required autocomplete="off">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="password" class="input" id="password" required>
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="submit" class="submit" value="Sign Up">
+                            
+                        </div>
+                        <div class="signin">
+                            <span>Already have an account? <a href="#">Log in here</a></span>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
