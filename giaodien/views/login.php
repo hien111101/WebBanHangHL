a97b285da00cde669d5eecef8d3348de823eff64
Hien Nguyen <nguyenhien111101@gmail.com>
2023-03-01 15:40:17 +0700

    css login and signup

--- giaodien/views/login.php
+++ giaodien/views/login.php
@@ -17,34 +17,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <script src="https://kit.fontawesome.com/1147679ae7.js" crossorigin="anonymous"></script>
+    <title>Trang chủ HL</title>
 </head>
 <body>
-<div class="login-page">
+<?php 
+  include_once("./shares/header.php");
+?>
+
+<div class="login-page" >
   <div class="container">
     <div class="row">
       <div class="col-md-6 login-image">
-        <img src="images/loginlogo.png" alt="Login Image">
+        <img src="../images/loginlogo.png" style="width:700px" alt="Login Image">
       </div>
-      <div class="col-md-6 login-form-container">
-        <h1>Login</h1>
+      <div class="col-md-6 login-form-container" >
+        <h1 style="text-align:center;">Login</h1>
         <form class="login-form" action="">
-          <div class="form-group">
-            <input type="email" class="form-control" id="email" placeholder="Email">
+          <div class="form-group" >
+            <input type="text" class="form-control" id="user" placeholder="UserName">
           </div>
           <div class="form-group">
             <input type="password" class="form-control" id="password" placeholder="Password">
           </div>
-          <div class="form-group form-check">
+          <div class="form-group form-check" >
             <input type="checkbox" class="form-check-input" id="remember-me">
             <label class="form-check-label" for="remember-me">Remember me</label>
           </div>
-          <button type="submit" class="btn btn-primary">Login</button>
-          <div class="form-group">
+          <div class="form-group-login" >
+            <button type="submit" style="height:30px;width:100px" class="btn btn-primary">Login</button>
+          </div>
+          <div class="form-group-login">
             <a href="#" class="forgot-password">Forgot password?</a>
           </div>
         </form>
@@ -52,6 +61,8 @@
     </div>
   </div>
 </div>
-
+<?php 
+  include_once("./shares/footer.php");
+?>
 </body>
 </html>
--- giaodien/views/signup.php
+++ giaodien/views/signup.php
@@ -1,52 +1,57 @@
-
-
-<!-- login form -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./signup.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <script src="https://kit.fontawesome.com/1147679ae7.js" crossorigin="anonymous"></script>
+    <title>Trang chủ HL</title>
 </head>
 <body>
-<div class="container-fluid bg-image">
-  <div class="row justify-content-center align-items-center vh-100">
-    <div class="col-md-6">
-      <div class="card">
-        <div class="card-header text-center">
-          <h4>Sign up</h4>
-        </div>
-        <div class="card-body">
-          <form>
-            <div class="form-group">
-              <label for="fullname">Full Name</label>
-              <input type="text" class="form-control" id="fullname" placeholder="Enter your full name">
-            </div>
-            <div class="form-group">
-              <label for="email">Email address</label>
-              <input type="email" class="form-control" id="email" placeholder="Enter your email address">
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" id="password" placeholder="Enter your password">
-            </div>
-            <div class="form-group">
-              <label for="confirm-password">Confirm Password</label>
-              <input type="password" class="form-control" id="confirm-password" placeholder="Confirm your password">
-            </div>
-            <button type="submit" class="btn btn-primary btn-block">Sign up</button>
-            <div class="text-center mt-2">
-              <a href="#">Forgot password?</a>
-            </div>
-          </form>
-        </div>
+<?php 
+  include_once("./shares/header.php");
+?>
+
+<div class="login-page" >
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 login-image">
+        <img src="../images/loginlogo.png" style="width:700px" alt="Login Image">
+      </div>
+      <div class="col-md-6 login-form-container" >
+        <h1 style="text-align:center;">Sign Up</h1>
+        <form class="login-form" action="">
+          <div class="form-group" >
+            <input type="text" class="form-control" id="user" placeholder="UserName">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" id="password" placeholder="Password">
+          </div>
+          <div class="form-group" >
+            <input type="Email" class="form-control" id="email" placeholder="Email">
+          </div><div class="form-group" >
+            <input type="text" class="form-control" id="phone" placeholder="Phone Number">
+          </div>
+          <div class="form-group" >
+            <input type="text" class="form-control" id="name" placeholder="Your Name">
+          </div>
+          <div class="form-group" >
+            <input type="text" class="form-control" id="address" placeholder="Address">
+          </div>
+          <div class="form-group-signup" >
+            <button type="submit" style="height:30px;width:100px" class="btn btn-primary">Sign Up</button>
+          </div>         
+        </form>
       </div>
     </div>
   </div>
 </div>
-
-
+<?php 
+  include_once("./shares/footer.php");
+?>
 </body>
 </html>
